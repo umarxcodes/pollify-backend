6ae9cb4a9730372ab56ec820c9a0a77ce6674f2f
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-07-28 09:23:21 +0500

    feat(biolar plate): i start the polling web app

--- src/modules/user/user.router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { register, login } from "./user.service.js";
-
-export const userRouter = express.Router();
-
-userRouter.post("/register", register);
-userRouter.post("/login", login);
